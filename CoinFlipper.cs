8867da9310be0683b6949e8a51338085ffc7dedf
bfields0805 <116600228+bfields0805@users.noreply.github.com>
2022-10-26 13:43:22 -0500

    Update CoinFlipper.cs

--- CoinFlipper.cs
+++ CoinFlipper.cs
@@ -3,33 +3,65 @@ using System;
 public class Program
 {
 	// Fields
+	public int test = 0;
+	
 	// Constructor
 	// Methods
+	//[access modifier] [modifier] [return type] [method name] ([parameters])
 	public static void Main()
 	{
 		Console.WriteLine("Starting Coin Flipper:");
 		
+		Console.WriteLine("Enter the number of coins to flip: ");
+		
 		string UserNumber = Console.ReadLine();
-		int Num = Int32.Parse(UserNumber);
+		int Num = 0;
 		
-		Console.WriteLine(Num);
+		try
+		{
+			Num = Int32.Parse(UserNumber);
+			if ( Num <= 0 )
+			{
+				throw new Exception("Argument may not be negative");
+			}
+		}
+		catch( InvalidOperationException e )
+		{
+			Console.WriteLine("A less specific catch: " + e.Message);
+		}
+		catch( ArgumentException e)
+		{
+			Console.WriteLine(e.Message);
+		}
+		catch( Exception e )
+		{
+			Console.WriteLine("The least specific catch: " + e.Message);
+		}
 		
+		Flip(Num);
+	}
+	
+	//[access modifier] [modifier] [return type] [method name] ([parameters])
+	public static void Flip(int Num)
+	{
 		var rand = new Random();
 		
 		for (int i = 0; i < Num; i++)
 		{
 			int coin = rand.Next(2);
-			
-			Console.WriteLine(coin);
-
-			if (coin == 0)
-			{
-				Console.WriteLine("Heads");	
-			}
-			else
-			{
-				Console.WriteLine("Tails");
-			}
+			HoT(coin);
+		}
+	}
+	
+	public static void HoT(int coin)
+	{
+		if (coin == 0)
+		{
+			Console.WriteLine("Heads");	
+		}
+		else
+		{
+			Console.WriteLine("Tails");
 		}
 	}
 }
